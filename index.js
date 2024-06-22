e7b048e708929081f989bd089a76bb1f9a0e3b08
Nguyen Hoangf <ntminhhoang96@gmail.com>
2024-06-22 09:31:03 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import React, { useCallback } from 'react'
+import {
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native'
+
+const MobilePos = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1 }}>
+        <Text>Hello World!!</Text>
+    </View>
+  )
+}
+
+
+module.exports = MobilePos
