3dd474adce1752d501107b2066eeb8ca1193b37c
Nguyen Hoangf <ntminhhoang96@gmail.com>
2024-06-28 11:33:38 +0700

    add image

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ import {
   TouchableOpacity,
   Text,
   View,
+  Dimensions
 } from 'react-native'
+export const backIcon = (require('./common/image/back-icon.png'))
 
 const MobilePos = ({ navigation }) => {
+  const width = Dimensions.get('window').width;
   return (
     <View style={{ flex: 1 }}>
+    <TouchableOpacity
+            onPress={() => {
+              navigation.goBack();
+            }}>
+            <Image
+              source={backIcon}
+              style={{
+                width: (24 * width) / 375,
+                height: (24 * width) / 375,
+              }}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
         <Text>Hello World!!</Text>
     </View>
   )
